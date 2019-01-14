1754fcdb8a79754f16ecebfed2484360af727d49
Victor Bruce <victorbruce82@gmail.com>
2019-01-14 13:40:09 +0000

    Updates

---
+++ precache-manifest.9e789cd6116b1ef351d9eed9fdd40d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "513d2b86aa8f153ba85f",
+    "url": "/static/css/main.1504687c.chunk.css"
+  },
+  {
+    "revision": "513d2b86aa8f153ba85f",
+    "url": "/static/js/main.513d2b86.chunk.js"
+  },
+  {
+    "revision": "96da7601cfecd8d4e58a",
+    "url": "/static/css/1.2d01d970.chunk.css"
+  },
+  {
+    "revision": "96da7601cfecd8d4e58a",
+    "url": "/static/js/1.96da7601.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9df1c118f2d49b251189fa03d1da658b",
+    "url": "/index.html"
+  }
+];
