8fbe928615e19f32962bc09430ace33db53ec484
Victor Bruce <victorbruce82@gmail.com>
2019-01-30 13:55:54 +0000

    Styled the navigation bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+// import Blog from "./components/Blog/Blog";
+// import Projects from "./components/Projects/Projects";
+// import Home from "./components/Home/Home";
+import "./assets/style.css";
 
 class App extends Component {
   render() {
     return (
       <main>
-        <nav>
-          <Link to="/">About</Link>
-          <Link to="/projects">Projects</Link>
-          <Link to="/blog">Blog</Link>
-        </nav>
+          <Navbar />
+          {/* <div>
+          <Switch>
+            <Route path="/blog" component={Blog} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </div> */}
       </main>
     );
   }
