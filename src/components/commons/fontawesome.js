1210c26f0b0562b3495dcd9b5eeac22531369ac3
Victor Bruce <victorbruce82@gmail.com>
2019-02-12 11:19:46 +0000

    Updates

--- src/components/commons/fontawesome.js
+++
@@ -1,7 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee, faCheckSquare} from '@fortawesome/free-solid-svg-icons'
-
-export default library.add(
-    faCoffee,
-    faCheckSquare
-)
